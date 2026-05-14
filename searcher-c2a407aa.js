8c7a75b0ae5c0033bfc7a8a83042693b435f0460
meister-qcom <meister-qcom@users.noreply.github.com>
2026-05-14 19:44:21 +0000

    deploy: 09571feaa958be5007e9c43770ec3cd69e55e724

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d42e417b.js',
+                path_to_root + 'searchindex-f3e0c153.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
